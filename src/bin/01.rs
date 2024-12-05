ce301c47d3d3f15cf578639efe80e52689ac10aa
Drumstickz64 <gearman533@gmail.com>
2024-12-05 17:54:19 +0300

    complete day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 advent_of_code::solution!(1);
@@ -14,14 +16,14 @@ pub fn part_one(input: &str) -> Option<u32> {
         })
         .unzip();
 
-    log::debug!("List 1 = {list1:?}");
-    log::debug!("List 2 = {list2:?}");
+    eprintln!("List 1 = {list1:?}");
+    eprintln!("List 2 = {list2:?}");
 
     list1.sort();
     list2.sort();
 
-    log::debug!("List 1 sorted = {list1:?}");
-    log::debug!("List 2 sorted = {list2:?}");
+    eprintln!("List 1 sorted = {list1:?}");
+    eprintln!("List 2 sorted = {list2:?}");
 
     Some(
         list1
@@ -35,7 +37,36 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (list1, list2): (Vec<i32>, Vec<i32>) = input
+        .lines()
+        .map(|line| line.split("   ").collect_tuple::<(_, _)>().unwrap())
+        .map(|(string1, string2)| {
+            (
+                string1.parse::<i32>().unwrap(),
+                string2.parse::<i32>().unwrap(),
+            )
+        })
+        .unzip();
+
+    eprintln!("List 1 = {list1:?}");
+    eprintln!("List 2 = {list2:?}");
+
+    let occurrence_map: HashMap<i32, i32> =
+        list2.into_iter().fold(HashMap::new(), |mut map, num| {
+            map.entry(num).and_modify(|count| *count += 1).or_insert(1);
+            map
+        });
+
+    eprintln!("occurrence map = {occurrence_map:#?}");
+
+    Some(
+        list1
+            .into_iter()
+            .map(|num| num * occurrence_map.get(&num).unwrap_or(&0))
+            .sum::<i32>()
+            .try_into()
+            .unwrap(),
+    )
 }
 
 #[cfg(test)]
@@ -51,6 +82,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }

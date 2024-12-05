f2c1e376b3c2fe4a023dccf8626484d64bbce42d
Drumstickz64 <gearman533@gmail.com>
2024-12-06 01:03:28 +0300

    complete day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -3,12 +3,12 @@ use itertools::Itertools;
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    fn is_valid_report(report: &str) -> bool {
+    fn is_safe_report(report: &str) -> bool {
         let mut prev_diff = i32::MAX;
-        for (x, y) in report.split_whitespace().tuple_windows() {
-            let x: i32 = x.parse().unwrap();
-            let y: i32 = y.parse().unwrap();
-            let diff = y - x;
+        for (level1, level2) in report.split_whitespace().tuple_windows() {
+            let level1: i32 = level1.parse().unwrap();
+            let level2: i32 = level2.parse().unwrap();
+            let diff = level2 - level1;
             if !(1..=3).contains(&diff.abs()) {
                 return false;
             }
@@ -31,7 +31,7 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(
         input
             .lines()
-            .filter(|report| is_valid_report(report))
+            .filter(|report| is_safe_report(report))
             .count()
             .try_into()
             .unwrap(),
@@ -39,7 +39,50 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    fn is_safe_report(report: &str) -> bool {
+        let mut prev_diff = i32::MAX;
+        let mut has_encounter_unsafe = false;
+        for (level1, level2) in report.split_whitespace().tuple_windows() {
+            let level1: i32 = level1.parse().unwrap();
+            let level2: i32 = level2.parse().unwrap();
+            let diff = level2 - level1;
+
+            match (!is_safe_level(diff, prev_diff), has_encounter_unsafe) {
+                (true, true) => return false,
+                (true, false) => has_encounter_unsafe = true,
+                _ => (),
+            }
+
+            prev_diff = diff;
+        }
+
+        true
+    }
+
+    fn is_safe_level(diff: i32, prev_diff: i32) -> bool {
+        if !(1..=3).contains(&diff.abs()) {
+            return false;
+        }
+
+        if prev_diff == i32::MAX {
+            return true;
+        }
+
+        if prev_diff.signum() != diff.signum() {
+            return false;
+        }
+
+        true
+    }
+
+    Some(
+        input
+            .lines()
+            .filter(|report| is_safe_report(report))
+            .count()
+            .try_into()
+            .unwrap(),
+    )
 }
 
 #[cfg(test)]
@@ -55,6 +98,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }

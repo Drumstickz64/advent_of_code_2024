881b5118f38864a303bfeba0b0f28905713ed95c
Drumstickz64 <gearman533@gmail.com>
2024-12-06 21:48:55 +0300

    complete day 3 part 1

---
+++ src/bin/03.rs
@@ -0,0 +1,40 @@
+use regex::Regex;
+
+advent_of_code::solution!(3);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    Some(
+        re.captures_iter(input)
+            .map(|c| {
+                let (_, [num1, num2]) = c.extract();
+                let num1: u32 = num1.parse().unwrap();
+                let num2: u32 = num2.parse().unwrap();
+
+                num1 * num2
+            })
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(161));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
